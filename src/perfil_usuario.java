5f5aa9b8ee739aee8dc92d4129ef7e4091c3c254
jp123468 <jose.panchi@epn.edu.ec>
2023-02-04 17:22:03 -0500

    cambios perfil usuario

--- src/perfil_usuario.java
+++ src/perfil_usuario.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class perfil_usuario extends JFrame{
+public class perfil_usuario extends JFrame {
     public JPanel perfil1;
     private JButton calculadoraDeDivisasButton;
     private JButton calculadoraIMCButton;
@@ -12,30 +12,33 @@ public class perfil_usuario extends JFrame{
     private String Dato;
 
     public perfil_usuario() {
-    calculadoraIMCButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFrame frame1 = new JFrame("Calculadora IMC");
-            frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            frame1.pack();
-            frame1.setSize(400, 400);
-            frame1.setLocationRelativeTo(null);
-            frame1.setVisible(true);
-        }
-    });
-    calculadoraDeDivisasButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            JFrame frame1 = new JFrame("Calculadora Divisas");
-            frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            frame1.pack();
-            frame1.setSize(400, 400);
-            frame1.setLocationRelativeTo(null);
-            frame1.setVisible(true);
-        }
-    });
-
-}
+        calculadoraIMCButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1 = new JFrame("Calculadora IMC");
+                calculadoraIMC f1 = new calculadoraIMC();
+                frame1.setContentPane(f1.p1);
+                frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                frame1.pack();
+                frame1.setSize(400, 400);
+                frame1.setLocationRelativeTo(null);
+                frame1.setVisible(true);
+            }
+        });
+        calculadoraDeDivisasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1 = new JFrame("Calculadora Divisas");
+                ConversorDivisas f1 = new ConversorDivisas();
+                frame1.setContentPane(f1.divisasPanel);
+                frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                frame1.pack();
+                frame1.setSize(400, 400);
+                frame1.setLocationRelativeTo(null);
+                frame1.setVisible(true);
+            }
+        });
+    }
     public void setDato(String Dato){
         this.Dato = Dato;
         UsuarioIng.setText(Dato);
