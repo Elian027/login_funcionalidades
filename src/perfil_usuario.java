df453d11e0d797828c2c4a085bee9f108451180a
jp123468 <jose.panchi@epn.edu.ec>
2023-02-05 23:23:41 -0500

    Error al ingresar a divisas

--- src/perfil_usuario.java
+++ src/perfil_usuario.java
@@ -2,20 +2,24 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class perfil_usuario extends JFrame {
+public class perfil_usuario extends JFrame{
     public JPanel perfil1;
     private JButton calculadoraDeDivisasButton;
     private JButton conversor_Unid_Button;
     public JLabel UsuarioIng;
     public JLabel Jlabel_Nombre;
-    public JLabel Jlabelfoto;
+    private JLabel Jlabelfoto;
+    private JButton calculadoraIMCButton;
+    private JLabel JLabelCod;
     private String Foto;
 
     private String Dato;
     private  String Dato1;
+    private String Dato2;
+
 
     public perfil_usuario() {
-        conversor_Unid_Button.addActionListener(new ActionListener() {
+        calculadoraIMCButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JFrame frame1 = new JFrame("Calculadora IMC");
@@ -23,7 +27,20 @@ public class perfil_usuario extends JFrame {
                 frame1.setContentPane(f1.p1);
                 frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
                 frame1.pack();
-                frame1.setSize(400, 400);
+                frame1.setSize(600, 600);
+                frame1.setLocationRelativeTo(null);
+                frame1.setVisible(true);
+            }
+        });
+        conversor_Unid_Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1 = new JFrame("Conversor Unidades");
+                ConvertidorUnidades f1 = new ConvertidorUnidades();
+                frame1.setContentPane(f1.conversorPanel);
+                frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                frame1.pack();
+                frame1.setSize(600, 600);
                 frame1.setLocationRelativeTo(null);
                 frame1.setVisible(true);
             }
@@ -36,11 +53,12 @@ public class perfil_usuario extends JFrame {
                 frame1.setContentPane(f1.divisasPanel);
                 frame1.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
                 frame1.pack();
-                frame1.setSize(400, 400);
+                frame1.setSize(600, 600);
                 frame1.setLocationRelativeTo(null);
                 frame1.setVisible(true);
             }
         });
+
     }
     public void setDato(String Dato){
         this.Dato = Dato;
@@ -51,7 +69,10 @@ public class perfil_usuario extends JFrame {
         this.Dato = Dato1;
         Jlabel_Nombre.setText(Dato1);
     }
-
+    public void setDato2(String Dato2){
+        this.Dato = Dato2;
+        JLabelCod.setText(Dato2);
+    }
     public void setFoto(String Foto) {
         this.Foto = Foto;
         Icon image = new ImageIcon(new ImageIcon(getClass().getClassLoader().getResource(Foto)).getImage().getScaledInstance(100, 100, 0));
